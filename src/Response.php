2fa0e2502db6f7a213f09b5a54a418e02722b7bb
Eric Abouaf <eric.abouaf@gmail.com>
2024-08-23 15:02:08 +0200

    Remove useless stuff

--- src/Response.php
+++ src/Response.php
@@ -9,32 +9,11 @@ class Response
     public readonly ParameterBag $parameters;
     public readonly Request $request;
 
-    //private bool $isError = false;
-    //private ?string $errorMessage = null;
-
     public function __construct(Request $request, array $parameters = [])
     {
         $this->request = $request;
         $this->parameters = new ParameterBag($parameters);
     }
-
-    /* public function getErrorMessage(): ?string
-    {
-        return $this->errorMessage;
-    }
-
-    public static function error(Request $request, string $errorMessage): self
-    {
-        $r = new self($request);
-        $r->isError = true;
-        $r->errorMessage = $errorMessage;
-        return $r;
-    }
-
-    public function isError(): bool
-    {
-        return $this->isError;
-    }*/
 
     public function copy(string $sourceParam, string $destParam): void
     {
