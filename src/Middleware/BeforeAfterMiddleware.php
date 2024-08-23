994bcfd913cc5262b9073d2ae9e18b83c59feb4e
Eric Abouaf <eric.abouaf@gmail.com>
2024-08-23 14:22:40 +0200

    BeforeAfterMiddleware

--- src/Middleware/BeforeAfterMiddleware.php
+++ src/Middleware/BeforeAfterMiddleware.php
@@ -3,8 +3,20 @@
 namespace Middl\Middleware;
 
 use Middl\AbstractMiddleware;
+use Middl\Request;
+use Middl\Response;
 
-class BeforeAfterMiddleware extends AbstractMiddleware
+abstract class BeforeAfterMiddleware extends AbstractMiddleware
 {
+    public function __invoke(Request $request, callable $next): Response
+    {
+        $this->before($request);
 
+        $response = $next($request);
+
+        return $this->after($request, $response);
+    }
+
+    abstract public function before(Request $request): Request;
+    abstract public function after(Request $request, Response $response): Response;
 }
